d3e10d928474e9752af7058bc344ba6216513fc9
Rahul Agarwal <rahulagdev@gmail.com>
2024-02-14 11:05:04 +0530

    Setting up environment

---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+import styles from "./homepage.module.css";
+
+export default function Home() {
+  return <div>
+  <h1>title</h1>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, ad rem illum soluta pariatur voluptatem commodi numquam! Perspiciatis nobis quibusdam ut beatae esse maxime perferendis culpa consequatur. Expedita, possimus repellat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quasi, totam veniam sit dolore suscipit quidem ut tempore ullam distinctio soluta temporibus sapiente accusantium magni nam molestias at porro voluptatibus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam assumenda eligendi itaque ullam adipisci repellendus exercitationem nostrum eaque quia! Vero cum rem veniam nisi. Iure fugiat debitis maxime provident eos! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit, quasi. Illo officiis, voluptatem officia eligendi reprehenderit sapiente recusandae magnam incidunt quaerat eaque soluta perferendis aliquam eveniet error rerum excepturi delectus.</div>;
+}

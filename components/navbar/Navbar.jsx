cefbb168e54e3cff6697e913c0660168ce911716
Rahul Agarwal <rahulagdev@gmail.com>
2024-02-14 21:23:19 +0530

    Adding the Featured section

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,45 +1,38 @@
-'use client'
-import React from 'react'
-import styles from './navbar.module.css'
-import Image from 'next/image'
-import Link from 'next/link'
-import AuthLinks from '../AuthLinks/AuthLinks'
-import ThemeToggle from '../themeToggle/ThemeToggle'
+"use client";
+import React from "react";
+import styles from "./navbar.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import AuthLinks from "../AuthLinks/AuthLinks";
+import ThemeToggle from "../themeToggle/ThemeToggle";
 const Navbar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.social}>
-        <Image 
-          src='/facebook.png'
-          alt='facebook'
-          width={24}
-          height={24}
-        />
-        <Image 
-          src='/instagram.png'
-          alt='instagram'
-          width={24}
-          height={24}
-        />
-        <Image 
-          src='/youtube.png'
-          alt='youtube'
-          width={24}
-          height={24}
-        />
+        <Link href="https://github.com/rahulagdev" target="_blank">
+          <Image src="/facebook.png" alt="facebook" width={24} height={24} />
+        </Link>
+        <Link href="https://instagram.com/rahul.ag_ig" target="_blank">
+          <Image src="/instagram.png" alt="instagram" width={24} height={24} />
+        </Link>
+        <Image src="/youtube.png" alt="youtube" width={24} height={24} />
       </div>
-      <div className={styles.logo}>
-        BlogIt
-      </div>
+      <div className={styles.logo}>BlogIt</div>
       <div className={styles.links}>
-      <ThemeToggle />
-        <Link href='/' className={styles.link}>Homepage</Link>
-        <Link href='/' className={styles.link}>Contact</Link>
-        <Link href='/' className={styles.link}>About</Link>
+        <ThemeToggle />
+        <Link href="/" className={styles.link}>
+          Homepage
+        </Link>
+        <Link href="/" className={styles.link}>
+          Contact
+        </Link>
+        <Link href="/" className={styles.link}>
+          About
+        </Link>
         <AuthLinks />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

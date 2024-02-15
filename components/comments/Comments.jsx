3e050a0ac8c75d81b8ec411ce378662414672ce3
Rahul Agarwal <rahulagdev@gmail.com>
2024-02-15 21:05:19 +0530

    Adding login route and style

---
+++ components/comments/Comments.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+import styles from "./comments.module.css";
+import Link from "next/link";
+import Image from "next/image";
+const Comments = () => {
+  const status = "authenticated";
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}></h1>
+        {status === "authenticated" ? (
+          <div className={styles.write}>
+            <textarea
+              placeholder="Write a comment ..."
+              className={styles.input}
+            />
+            <button className={styles.button}>Send</button>
+          </div>
+        ) : (
+          <Link href="/login">Login to write a comment </Link>
+        )}
+        <div className={styles.comments}>
+          <div className={styles.comment}>
+            <div className={styles.user}>
+              <Image
+                src="/p1.jpeg"
+                alt=""
+                width={50}
+                height={50}
+                className={styles.image}
+              />
+              <div className={styles.userInfo}>
+                <span className={styles.username}>John Doe</span>
+                <span className={styles.date}>15.2.2024</span>
+              </div>
+            </div>
+            <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa repudiandae debitis vitae quidem laudantium vel voluptatibus impedit corporis, quas.</p>
+          </div>
+          <div className={styles.comment}>
+            <div className={styles.user}>
+              <Image
+                src="/p1.jpeg"
+                alt=""
+                width={50}
+                height={50}
+                className={styles.image}
+              />
+              <div className={styles.userInfo}>
+                <span className={styles.username}>John Doe</span>
+                <span className={styles.date}>15.2.2024</span>
+              </div>
+            </div>
+            <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa repudiandae debitis vitae quidem laudantium vel voluptatibus impedit corporis, quas.</p>
+          </div>
+          <div className={styles.comment}>
+            <div className={styles.user}>
+              <Image
+                src="/p1.jpeg"
+                alt=""
+                width={50}
+                height={50}
+                className={styles.image}
+              />
+              <div className={styles.userInfo}>
+                <span className={styles.username}>John Doe</span>
+                <span className={styles.date}>15.2.2024</span>
+              </div>
+            </div>
+            <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa repudiandae debitis vitae quidem laudantium vel voluptatibus impedit corporis, quas.</p>
+          </div>
+          <div className={styles.comment}>
+            <div className={styles.user}>
+              <Image
+                src="/p1.jpeg"
+                alt=""
+                width={50}
+                height={50}
+                className={styles.image}
+              />
+              <div className={styles.userInfo}>
+                <span className={styles.username}>John Doe</span>
+                <span className={styles.date}>15.2.2024</span>
+              </div>
+            </div>
+            <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa repudiandae debitis vitae quidem laudantium vel voluptatibus impedit corporis, quas.</p>
+          </div>
+          <div className={styles.comment}>
+            <div className={styles.user}>
+              <Image
+                src="/p1.jpeg"
+                alt=""
+                width={50}
+                height={50}
+                className={styles.image}
+              />
+              <div className={styles.userInfo}>
+                <span className={styles.username}>John Doe</span>
+                <span className={styles.date}>15.2.2024</span>
+              </div>
+            </div>
+            <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa repudiandae debitis vitae quidem laudantium vel voluptatibus impedit corporis, quas.</p>
+          </div>
+        </div>
+      
+    </div>
+  );
+};
+
+export default Comments;

41f16d4d516fad95689807466bd67026e255eb88
Rahul Agarwal <rahulagdev@gmail.com>
2024-02-16 10:30:03 +0530

    Adding prisma adapter

--- components/AuthLinks/AuthLinks.jsx
+++ components/AuthLinks/AuthLinks.jsx
@@ -2,16 +2,18 @@
 import React, { useState } from "react";
 import styles from "./authLinks.module.css";
 import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
 const AuthLinks = () => {
   const [open, setOpen] = useState(false);
-  const status = "authenticated";
-  return (
+  const {data, status} = useSession();
+
+   return (
     // <div className={styles.container}>
 
     // </div>
 
     <>
-      {status === "notauthenticated" ? (
+      {status === "unauthenticated" ? (
         <Link href="/login" className={styles.link}>
           Login
         </Link>
@@ -20,7 +22,7 @@ const AuthLinks = () => {
           <Link href="/write" className={styles.link}>
             Write
           </Link>
-          <span className={styles.link}>Logout</span>
+          <span onClick={signOut} className={styles.link}>Logout</span>
         </>
       )}
       <div className={styles.burger} onClick={() => setOpen((prev) => !prev)}>
@@ -38,7 +40,7 @@ const AuthLinks = () => {
             ) : (
               <>
                 <Link href="/write">Write</Link>
-                <span>Logout</span>
+                <span onClick={signOut}>Logout</span>
               </>
             )}
           </div>
